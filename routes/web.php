f4c203aeb6bf6846c92a1f6adc2e16cda05afd74
= <=>
2025-02-19 10:26:37 +0700

    Percobaan 1: Basic Routing Parameters

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return "Hello";
+});
+
+Route::get('/world', function () {
+    return "World";
+});
+
+Route::get('/', function () {
+    return "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    return "NIM: 2341760095 <br> Nama: Susilowati Syafa Adilah";
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{postId}/comments/{commentId}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-" . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID " . $id;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
